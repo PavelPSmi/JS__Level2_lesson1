903a2abead82929149910ea1637c44a70d446e12
PavelPSmi <101994418+PavelPSmi@users.noreply.github.com>
2022-08-05 18:18:38 +0300

    JS__Level2_lesson1
    JS__Level2_lesson1

---
+++ JS__file/app.js
@@ -0,0 +1,23 @@
+const img = 'img';
+const main = 'main';
+const goodListCtg = "good-list";
+const renderProduct = (card) => `
+                <div class="goods-item">
+                    <div>
+                        <img src="./${img}/${main}/${goodListCtg}/${card.image}" alt="BeardManitemCard">
+                    </div>
+                    <div class="goods-item-text">
+                        <h3>${card.title}</h3>
+                        <p>${card.price} руб.</p>
+                        <button class="cart-button data-id${card.id}">купить</button>
+                    </div>
+                </div>
+`;
+
+const renderPage = (list) => {
+  const productList = list.map((card) => renderProduct(card)).join('');
+  document.querySelector(".goods-list").innerHTML = productList;
+};
+renderPage(products);
+
+//элементы array по умолчанию разделены запятой. join() позволяет поставить между элементами массива любой другой элемент, какой мы ему передадим
---
+++ JS__file/list__product.js
@@ -0,0 +1,39 @@
+
+const products = [
+  {
+    id: 1,
+    title: "product1",
+    price: 15000,
+    image: "BeardManitemCard.svg",
+  },
+  {
+    id: 2,
+    title: "product2",
+    price: 25000,
+    image: "BeardManitemCard.svg",
+  },
+  {
+    id: 3,
+    title: "product3",
+    price: 55000,
+    image: "BeardManitemCard.svg",
+  },
+  {
+    id: 4,
+    title: "product4",
+    price: 77000,
+    image: "BeardManitemCard.svg",
+  },
+  {
+    id: 5,
+    title: "product5",
+    price: 700,
+    image: "BeardManitemCard.svg",
+  },
+  {
+    id: 6,
+    title: "product6",
+    price: 400,
+    image: "BeardManitemCard.svg",
+  },
+];
